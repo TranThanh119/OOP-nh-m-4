d8781da8abc1ee331c165961cd4a1623416eb92c
BuiDuc21012492 <96609660+BuiDuc21012492@users.noreply.github.com>
2024-10-16 14:33:53 +0700

    Add files via upload

---
+++ QLSV/QuanlySinhVien/src/quanlysinhvien/QuanLySinhVien.java
@@ -0,0 +1,17 @@
+
+package quanlysinhvien;
+
+import views.loginFrame;
+
+public class QuanLySinhVien {
+
+    public QuanLySinhVien() {
+        loginFrame loginFrame = new loginFrame();
+        loginFrame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        QuanLySinhVien QLMain = new QuanLySinhVien();
+    }
+    
+}
