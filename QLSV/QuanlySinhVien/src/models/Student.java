705b8dec852f1e31dc178489e2da38b997e94638
BuiDuc21012492 <96609660+BuiDuc21012492@users.noreply.github.com>
2024-10-16 14:32:48 +0700

    Add files via upload

---
+++ QLSV/QuanlySinhVien/src/models/Student.java
@@ -0,0 +1,92 @@
+package models;
+
+public class Student {
+    private int stID = 0;
+    private String stName = "";
+    private String stBirth = "";
+    private String stGender = "";
+    private int stPhone = 0;
+    private String stClass = "";
+    private float stPoint = 0;
+    private int stPonit_R = 0;
+    private byte[] hinhanh = null;
+
+    public Student() {
+    }
+
+    public Student(int stID, String stName, String stBirth, String stGender, int stPhone, String stClass, float stPoint, int stPonit_R) {
+        this.stID = stID;
+        this.stName = stName;
+        this.stBirth = stBirth;
+        this.stGender = stGender;
+        this.stPhone = stPhone;
+        this.stClass = stClass;
+        this.stPoint = stPoint;
+        this.stPonit_R = stPonit_R;
+    }
+
+    public int getStID() {
+        return stID;
+    }
+
+    public void setStID(int stID) {
+        this.stID = stID;
+    }
+
+    public String getStName() {
+        return stName;
+    }
+
+    public void setStName(String stName) {
+        this.stName = stName;
+    }
+
+    public String  getStBirth() {
+        return stBirth;
+    }
+
+    public void setStBirth(String stBirth) {
+        this.stBirth = stBirth;
+    }
+
+    public String getStGender() {
+        return stGender;
+    }
+
+    public void setStGender(String stGender) {
+        this.stGender = stGender;
+    }
+
+    public int getStPhone() {
+        return stPhone;
+    }
+
+    public void setStPhone(int stPhone) {
+        this.stPhone = stPhone;
+    }
+
+    public String getStClass() {
+        return stClass;
+    }
+
+    public void setStClass(String stClass) {
+        this.stClass = stClass;
+    }
+
+    public float getStPoint() {
+        return stPoint;
+    }
+
+    public void setStPoint(float stPoint) {
+        this.stPoint = stPoint;
+    }
+
+    public int getStPonit_R() {
+        return stPonit_R;
+    }
+
+    public void setStPonit_R(int stPonit_R) {
+        this.stPonit_R = stPonit_R;
+    }
+    
+}
---
+++ QLSV/QuanlySinhVien/src/models/User.java
@@ -0,0 +1,41 @@
+package models;
+
+public class User {
+    private int userID;
+    private String userName;
+    private String passWord;
+    public User() {
+    }
+//Khởi tạo có tham số
+    public User(int userID, String userName, String passWord) {
+        this.userID = userID;
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+//Phương thức get và set
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUseID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    
+    
+}
